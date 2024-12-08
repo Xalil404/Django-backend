d14019b1a0e8ca82f41609f9bb107c8f5ce41712
Xalil404 <kareemxalil@gmail.com>
2024-12-08 11:25:42 +0000

    Deleted unused code

--- GoogleAuth/views.py
+++ GoogleAuth/views.py
@@ -1,20 +1,4 @@
-# views.py (for Web Pop-up Flow)
-'''
-import json
-from google.oauth2 import id_token
-from google.auth.transport import requests
-from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
-from django.conf import settings
-from rest_framework.authtoken.models import Token
-
-
-# (for Web Redirect Flow)
-import requests
-import logging
-from google.auth.transport.requests import Request
-'''
+# views.py 
 import json
 from google.oauth2 import id_token
 from google.auth.transport.requests import Request
@@ -26,6 +10,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+# View for pop-up ux
 @csrf_exempt
 def google_auth(request):
     if request.method == 'POST':
@@ -62,44 +47,6 @@ def google_auth(request):
             return JsonResponse({'error': str(e)}, status=400)
 
     return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
-
-
-# views.py (for Web Pop-up Flow)
-'''
-@csrf_exempt
-def google_auth(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        token = body.get('token')
-
-        try:
-            # Verify the token with Google
-            idinfo = id_token.verify_oauth2_token(token, requests.Request(), "26271032790-djnijd5ookmvg0d58pneg2l8l6bdgvbn.apps.googleusercontent.com")
-            email = idinfo['email']
-            first_name = idinfo.get('given_name', '')
-            last_name = idinfo.get('family_name', '')
-
-            # Check if user exists; if not, create a new one
-            user, created = User.objects.get_or_create(
-                email=email,
-                defaults={
-                    'username': email.split('@')[0],
-                    'first_name': first_name,
-                    'last_name': last_name,
-                }
-            )
-
-            # Create or get a token for the user
-            from rest_framework.authtoken.models import Token
-            token, _ = Token.objects.get_or_create(user=user)
-
-            return JsonResponse({'token': token.key}, status=200)
-
-        except ValueError:
-            return JsonResponse({'error': 'Invalid token'}, status=400)
-
-    return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
-'''
 
 
 # (for Web Redirect Flow - this code below is for debugging and getting the proper token)
@@ -169,6 +116,7 @@ def google_auth_redirect(request):
     return JsonResponse({'error': 'Only POST method is allowed'}, status=405)
 '''
 
+# (for Web Redirect Flow )
 @csrf_exempt
 def google_auth_redirect(request):
     if request.method == 'POST':
